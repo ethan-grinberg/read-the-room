23f28d2653edb8abdc10592130d5d4b8e7731936
ethanbg2 <ethanbg2@illinois.edu>
2021-06-27 15:10:52 -0500

    initial commit

---
+++ process_audio.py
@@ -0,0 +1,30 @@
+import struct
+import pyaudio
+import math
+
+def rms(data_):
+    count = len(data_) / 2
+    format_ = "%dh" % count
+    shorts = struct.unpack(format_, data_)
+    sum_squares = 0.0
+    for sample in shorts:
+        n = sample * (1.0 / 32768)
+        sum_squares += n * n
+
+    return math.sqrt(sum_squares / count)
+
+
+CHUNK = 1024
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16,
+                channels=2,
+                rate=44100,
+                input=True,
+                frames_per_buffer=CHUNK)
+while True:
+    data = stream.read(CHUNK)
+    conversion = rms(data) * 4
+    if conversion > 1:
+        conversion = 1
+
+    print(conversion)
